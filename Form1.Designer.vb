ace1758e0f320ecb90c902dfd0a5371e8ecdf169
Libor Knappek <libor.knappek@gmail.com>
2021-01-08 23:59:13 +0100

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,362 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblQuantity = New System.Windows.Forms.Label()
+        Me.tbItem1 = New System.Windows.Forms.TextBox()
+        Me.tbItem2 = New System.Windows.Forms.TextBox()
+        Me.tbItem4 = New System.Windows.Forms.TextBox()
+        Me.tbItem3 = New System.Windows.Forms.TextBox()
+        Me.tbQuantity1 = New System.Windows.Forms.TextBox()
+        Me.tbQuantity2 = New System.Windows.Forms.TextBox()
+        Me.tbQuantity3 = New System.Windows.Forms.TextBox()
+        Me.tbQuantity4 = New System.Windows.Forms.TextBox()
+        Me.lblItem = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.tbSubtotal = New System.Windows.Forms.TextBox()
+        Me.tbTotal = New System.Windows.Forms.TextBox()
+        Me.tbTax = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.tbPrice4 = New System.Windows.Forms.TextBox()
+        Me.tbPrice3 = New System.Windows.Forms.TextBox()
+        Me.tbPrice2 = New System.Windows.Forms.TextBox()
+        Me.tbPrice1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblQuantity
+        '
+        Me.lblQuantity.AutoSize = True
+        Me.lblQuantity.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblQuantity.Location = New System.Drawing.Point(286, 23)
+        Me.lblQuantity.Name = "lblQuantity"
+        Me.lblQuantity.Size = New System.Drawing.Size(100, 29)
+        Me.lblQuantity.TabIndex = 0
+        Me.lblQuantity.Text = "Quantity"
+        '
+        'tbItem1
+        '
+        Me.tbItem1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbItem1.Location = New System.Drawing.Point(35, 55)
+        Me.tbItem1.Name = "tbItem1"
+        Me.tbItem1.Size = New System.Drawing.Size(202, 30)
+        Me.tbItem1.TabIndex = 1
+        Me.tbItem1.Text = "Notebook"
+        '
+        'tbItem2
+        '
+        Me.tbItem2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbItem2.Location = New System.Drawing.Point(35, 91)
+        Me.tbItem2.Name = "tbItem2"
+        Me.tbItem2.Size = New System.Drawing.Size(202, 30)
+        Me.tbItem2.TabIndex = 1
+        Me.tbItem2.Text = "Pencil"
+        '
+        'tbItem4
+        '
+        Me.tbItem4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbItem4.Location = New System.Drawing.Point(35, 163)
+        Me.tbItem4.Name = "tbItem4"
+        Me.tbItem4.Size = New System.Drawing.Size(202, 30)
+        Me.tbItem4.TabIndex = 1
+        Me.tbItem4.Text = "Cookies"
+        '
+        'tbItem3
+        '
+        Me.tbItem3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbItem3.Location = New System.Drawing.Point(35, 127)
+        Me.tbItem3.Name = "tbItem3"
+        Me.tbItem3.Size = New System.Drawing.Size(202, 30)
+        Me.tbItem3.TabIndex = 1
+        Me.tbItem3.Text = "Laptop"
+        '
+        'tbQuantity1
+        '
+        Me.tbQuantity1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbQuantity1.Location = New System.Drawing.Point(286, 54)
+        Me.tbQuantity1.Name = "tbQuantity1"
+        Me.tbQuantity1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbQuantity1.Size = New System.Drawing.Size(100, 30)
+        Me.tbQuantity1.TabIndex = 2
+        '
+        'tbQuantity2
+        '
+        Me.tbQuantity2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbQuantity2.Location = New System.Drawing.Point(286, 90)
+        Me.tbQuantity2.Name = "tbQuantity2"
+        Me.tbQuantity2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbQuantity2.Size = New System.Drawing.Size(100, 30)
+        Me.tbQuantity2.TabIndex = 2
+        '
+        'tbQuantity3
+        '
+        Me.tbQuantity3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbQuantity3.Location = New System.Drawing.Point(286, 126)
+        Me.tbQuantity3.Name = "tbQuantity3"
+        Me.tbQuantity3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbQuantity3.Size = New System.Drawing.Size(100, 30)
+        Me.tbQuantity3.TabIndex = 2
+        '
+        'tbQuantity4
+        '
+        Me.tbQuantity4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbQuantity4.Location = New System.Drawing.Point(286, 162)
+        Me.tbQuantity4.Name = "tbQuantity4"
+        Me.tbQuantity4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbQuantity4.Size = New System.Drawing.Size(100, 30)
+        Me.tbQuantity4.TabIndex = 2
+        '
+        'lblItem
+        '
+        Me.lblItem.AutoSize = True
+        Me.lblItem.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblItem.Location = New System.Drawing.Point(40, 23)
+        Me.lblItem.Name = "lblItem"
+        Me.lblItem.Size = New System.Drawing.Size(59, 29)
+        Me.lblItem.TabIndex = 0
+        Me.lblItem.Text = "Item"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblPrice.Location = New System.Drawing.Point(423, 23)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(129, 29)
+        Me.lblPrice.TabIndex = 0
+        Me.lblPrice.Text = "Price Each"
+        Me.lblPrice.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(599, 54)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(132, 30)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(599, 91)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(132, 30)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(599, 126)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox3.Size = New System.Drawing.Size(132, 30)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(599, 162)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox4.Size = New System.Drawing.Size(132, 30)
+        Me.TextBox4.TabIndex = 2
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(611, 22)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(120, 29)
+        Me.lblTotal.TabIndex = 0
+        Me.lblTotal.Text = "Item Total"
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'tbSubtotal
+        '
+        Me.tbSubtotal.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.tbSubtotal.Enabled = False
+        Me.tbSubtotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbSubtotal.Location = New System.Drawing.Point(599, 256)
+        Me.tbSubtotal.Name = "tbSubtotal"
+        Me.tbSubtotal.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbSubtotal.Size = New System.Drawing.Size(132, 30)
+        Me.tbSubtotal.TabIndex = 2
+        '
+        'tbTotal
+        '
+        Me.tbTotal.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.tbTotal.Enabled = False
+        Me.tbTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbTotal.Location = New System.Drawing.Point(599, 328)
+        Me.tbTotal.Name = "tbTotal"
+        Me.tbTotal.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbTotal.Size = New System.Drawing.Size(132, 30)
+        Me.tbTotal.TabIndex = 2
+        '
+        'tbTax
+        '
+        Me.tbTax.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.tbTax.Enabled = False
+        Me.tbTax.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbTax.Location = New System.Drawing.Point(599, 292)
+        Me.tbTax.Name = "tbTax"
+        Me.tbTax.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbTax.Size = New System.Drawing.Size(132, 30)
+        Me.tbTax.TabIndex = 2
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.btnCalculate.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnCalculate.Location = New System.Drawing.Point(286, 282)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(151, 40)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'tbPrice4
+        '
+        Me.tbPrice4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbPrice4.Location = New System.Drawing.Point(428, 163)
+        Me.tbPrice4.Name = "tbPrice4"
+        Me.tbPrice4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbPrice4.Size = New System.Drawing.Size(100, 30)
+        Me.tbPrice4.TabIndex = 2
+        '
+        'tbPrice3
+        '
+        Me.tbPrice3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbPrice3.Location = New System.Drawing.Point(428, 126)
+        Me.tbPrice3.Name = "tbPrice3"
+        Me.tbPrice3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbPrice3.Size = New System.Drawing.Size(100, 30)
+        Me.tbPrice3.TabIndex = 2
+        '
+        'tbPrice2
+        '
+        Me.tbPrice2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbPrice2.Location = New System.Drawing.Point(428, 91)
+        Me.tbPrice2.Name = "tbPrice2"
+        Me.tbPrice2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbPrice2.Size = New System.Drawing.Size(100, 30)
+        Me.tbPrice2.TabIndex = 2
+        '
+        'tbPrice1
+        '
+        Me.tbPrice1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.tbPrice1.Location = New System.Drawing.Point(428, 54)
+        Me.tbPrice1.Name = "tbPrice1"
+        Me.tbPrice1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbPrice1.Size = New System.Drawing.Size(100, 30)
+        Me.tbPrice1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.tbQuantity4)
+        Me.Controls.Add(Me.tbQuantity3)
+        Me.Controls.Add(Me.tbQuantity2)
+        Me.Controls.Add(Me.tbPrice4)
+        Me.Controls.Add(Me.tbPrice3)
+        Me.Controls.Add(Me.tbPrice2)
+        Me.Controls.Add(Me.tbTax)
+        Me.Controls.Add(Me.tbTotal)
+        Me.Controls.Add(Me.tbSubtotal)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.tbPrice1)
+        Me.Controls.Add(Me.tbQuantity1)
+        Me.Controls.Add(Me.tbItem3)
+        Me.Controls.Add(Me.tbItem4)
+        Me.Controls.Add(Me.tbItem2)
+        Me.Controls.Add(Me.tbItem1)
+        Me.Controls.Add(Me.lblItem)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.lblQuantity)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblQuantity As Label
+    Friend WithEvents tbItem1 As TextBox
+    Friend WithEvents tbItem2 As TextBox
+    Friend WithEvents tbItem4 As TextBox
+    Friend WithEvents tbItem3 As TextBox
+    Friend WithEvents tbQuantity1 As TextBox
+    Friend WithEvents tbQuantity2 As TextBox
+    Friend WithEvents tbQuantity3 As TextBox
+    Friend WithEvents tbQuantity4 As TextBox
+    Friend WithEvents lblItem As Label
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents tbSubtotal As TextBox
+    Friend WithEvents tbTotal As TextBox
+    Friend WithEvents tbTax As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents tbPrice4 As TextBox
+    Friend WithEvents tbPrice3 As TextBox
+    Friend WithEvents tbPrice2 As TextBox
+    Friend WithEvents tbPrice1 As TextBox
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Initiatize
+        Dim QuantityArray As Decimal() = {tbQuantity1.Text, tbQuantity2.Text, tbQuantity3.Text, tbQuantity4.Text}
+        Dim PriceArray As Decimal() = {tbPrice1.Text, tbPrice2.Text, tbPrice3.Text, tbPrice4.Text}
+        'Calculate
+        For Each i As Integer In Enumerable.Range(0, QuantityArray.Length)
+            QuantityArray(i) = QuantityArray(i) * PriceArray(i)
+        Next
+
+
+    End Sub
+End Class
